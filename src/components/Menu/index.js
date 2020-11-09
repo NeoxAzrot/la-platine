f42186366529864913e9a417e20c14fefb370dc9
NeoxAzrot1 <sami.lepays@gmail.com>
2020-11-09 21:52:05 +0100

    Settings IDE

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import styles from './Menu.module.css'
 
 const Menu = () => {
   return (
-    <aside>
-      <span>Menu</span>
-      <ul>
-        <li>Artistes</li>
-        <li>Albums</li>
-        <li>Crédits</li>
+    <nav className={styles.header}>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>
+          Premier composant
+        </li>
+        <li className={styles.menuItem}>
+          Styliser les composants
+        </li>
+        <li className={styles.menuItem}>
+          Hooks
+        </li>
       </ul>
-    </aside>
+    </nav>
   )
 }
 
