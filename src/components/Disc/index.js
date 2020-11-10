8596a7ff6b68b30a09e1b231df0ca51fd7494f1a
NeoxAzrot1 <sami.lepays@gmail.com>
2020-11-10 21:46:06 +0100

    Menu with svg good

--- src/components/Disc/index.js
+++ src/components/Disc/index.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import styles from './Disc.module.css'
 
 const Disc = () => {
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Menu.module.css'
+import HomeIcon from '../../icons/HomeIcon'
+import SearchIcon from '../../icons/SearchIcon'
+import AccountIcon from '../../icons/AccountIcon'
 
 const Menu = () => {
   return (
     <nav className={styles.header}>
       <ul className={styles.menu}>
         <li>
-          <NavLink to="/home" className={styles.menuItem} activeClassName={styles.active}>Disquaire</NavLink>
+          <NavLink to="/home" className={styles.menuItem} activeClassName={styles.active}>
+            <HomeIcon/>
+            Disquaire
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/search" className={styles.menuItem} activeClassName={styles.active}>Recherche</NavLink>
+          <NavLink to="/search" className={styles.menuItem} activeClassName={styles.active}>
+            <SearchIcon/>
+            Recherche
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/account" className={styles.menuItem} activeClassName={styles.active}>Compte</NavLink>
+          <NavLink to="/account" className={styles.menuItem} activeClassName={styles.active}>
+            <AccountIcon/>
+            Compte
+          </NavLink>
         </li>
       </ul>
     </nav>
