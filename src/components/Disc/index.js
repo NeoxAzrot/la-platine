6835bf0453a48ec74174974bfd63f6885917a07c
pop-hip <56161185+pop-hip@users.noreply.github.com>
2020-11-10 14:45:24 +0100

    ajout du component Disc, (a mettre en page)

---
+++ src/components/Disc/index.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import styles from './Disc.module.css'
+
+const Disc = () => {
+    return (
+
+        <div>
+            <img className={styles.Disc} src = './img/Vynile3.png'></img>
+        </div>
+    )
+}
+
+export default Disc
