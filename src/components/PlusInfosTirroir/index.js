d2881ab8918aebd356b657c51ad6e0050d406ad1
mariesmk <marie.simonek@mmibordeaux.com>
2020-11-12 17:12:56 +0100

    LinkSocialMedia and PlusInfosTirroir

--- src/components/PlusInfosTirroir/index.js
+++ src/components/PlusInfosTirroir/index.js
@@ -1,25 +1,45 @@
 import React from "react";
-import styles from './PlusInfosTirroir.module.css'
+import { makeStyles } from "@material-ui/core/styles"; 
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from "../../icons/HomeIcon"
+
+
+const useStyles = makeStyles((theme) => ({
+ background:{
+   backgroundColor:'#232323',
+ },
+ title:{
+  fontFamily:'Fugaz One',
+  color: '#F6F6F6',
+  fontSize:'25px',
+},
+text: {
+  fontFamily:'Montserrat',
+  color: '#F6F6F6',
+  fontSize:'16px',
+}
+}));
+
 const PlusInfosTirroir = (props) => {
+  const classes=useStyles()
+
   return (
     <div>
       <Accordion>
-        <AccordionSummary className = {styles.background}
+        <AccordionSummary className={classes.background}
           expandIcon={<HomeIcon/>}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
 
-        <Typography className={styles.title}>{props.title}</Typography>
+        <Typography className={classes.title}>{props.title}</Typography>
         </AccordionSummary>
 
-        <AccordionDetails className={styles.background}>
-        <Typography className={styles.text}>{props.text}</Typography>
+        <AccordionDetails className={classes.background}>
+        <Typography className={classes.text}>{props.text}</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
