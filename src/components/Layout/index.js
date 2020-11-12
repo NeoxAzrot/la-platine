81aac03e3e88be42b55b549babbe3f8dc2280e0c
NeoxAzrot1 <sami.lepays@gmail.com>
2020-11-12 15:00:31 +0100

    Player reduce and craco config and disc for everywhere

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Menu from 'components/Menu'
 import styles from './Layout.module.css'
+import ReducePlayer from 'components/ReducePlayer'
 
 const Layout = (props) => {
   const { children } = props
   return (
     <div className={styles.layout}>
-      <Menu />
       {children}
+      <ReducePlayer />
+      <Menu />
     </div>
   )
 }
