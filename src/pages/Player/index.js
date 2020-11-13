f23669918818f632ef2d4d534e8b218ef8f9a6c1
NeoxAzrot1 <sami.lepays@gmail.com>
2020-11-13 12:57:16 +0100

    Conflis netlify paulin

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -7,17 +7,15 @@ import BrasDeLecture from 'components/BrasDeLecture'
 import { Link } from 'react-router-dom'
 import LogoBanner from 'components/LogoBanner'
 
+let soundFrom = 'du Disquaire'
 
-var soundFrom = 'Disquaire'
-var gender1 = 'du'
-var gender2 = 'de la'
 const Player = () => {
   return (
     <Layout>
       <Title name="Lecteur"/>
       {/*la size modifie la size de la hauteur du logobanner seulement*/}
       <LogoBanner size={64}/>
-  <p className={styles.Listener}>Ecoute à partir {gender1} {soundFrom}</p>
+  <p className={styles.Listener}>Ecoute à partir {soundFrom}</p>
       <div className={styles.Disc}>
         <Disc size={355} image="../images/coverNepal.jpg"/>
       </div>
