904a97ce49c1ffe7d0654760181aac5d37d570b8
NeoxAzrot1 <sami.lepays@gmail.com>
2020-11-13 12:28:37 +0100

    Home page + reduce player svg animation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import styles from './Home.module.css'
 import Title from 'components/Title'
 import Disc from 'components/Disc'
 import { Link } from 'react-router-dom'
+import BrasDeLecture from 'components/BrasDeLecture'
 
 const Home = () => {
   return (
@@ -11,10 +12,18 @@ const Home = () => {
       <Title name="Disquaire"/>
       <div className={styles.container}>
         <Link to="/"><img className={styles.logo} src='../images/logo.png' alt='La Platine'/></Link>
-        <div>
-          <Disc size={165} image="../images/coverFlyzen.jpg"/>
-          <Link to="/player"><Disc size={329} image="../images/coverNepal.jpg"/></Link>
-          <Disc size={165} image="../images/coverKikesa.png"/>
+        <div className={styles.music}>
+          <div className={styles.cover}><Disc size={165} image="../images/coverFlyzen.jpg"/></div>
+          <div className={styles.cover}>
+            <Link to="/player"><Disc size={329} image="../images/coverNepal.jpg"/></Link>
+          </div>
+          <div className={styles.cover}><Disc size={165} image="../images/coverKikesa.png"/></div>
+        </div>
+        <BrasDeLecture/>
+        <div className={styles.infos}>
+          <h3 className={styles.title}>Babylone</h3>
+          <p className={styles.album}>444NUITS</p>
+          <p className={styles.artist}>Népal</p>
         </div>
       </div>
     </Layout>
